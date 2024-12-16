f48cabb73eecd44c0b69ee493e6fe27e3f0ecb68
Anushka Sadegaonkar <157234047+anushh13@users.noreply.github.com>
2024-12-16 22:42:26 +0530

    Update Arduino.Fire.Sensor.Alarm.Code.ino

--- Arduino.Fire.Sensor.Alarm.Code.ino
+++ Arduino.Fire.Sensor.Alarm.Code.ino
@@ -1,9 +1,3 @@
-/*
- * Author: Ashish Vegan (Labade) | https://www.ashishvegan.com | +91-9890345539
- * Subscribe YouTube Channel - Tech Vegan Click https://www.youtube.com/channel/UCs_dbtq_OF-0HxkAQnBGapA?sub_confirmation=1
- * For Customize Hardware & Software Projects Contact Me | Email: technologyvegan@gmail.com
- * Watch Complete Video: For Code Explanation
- */
 const int buzzerPin = 9;
 const int fireSensorPin = 8;
 void setup()
